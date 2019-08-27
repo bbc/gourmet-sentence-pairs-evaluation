d18751f77a7efb0bf6fa6fb3fcbca403a3f7950b
Susie Coleman <login@susiecoleman.me>
2019-08-27 16:07:55 +0100

    Deploy project to cloud using jenkins and cosmos

--- infrastructure/src/main.py
+++ infrastructure/src/main.py
@@ -1,4 +1,4 @@
 from cosmosTroposphere import CosmosTemplate
-t = CosmosTemplate(description="GoURMET Translation UI",
-                   project_name="gourmet", component_name="translation-ui")
+t = CosmosTemplate(description="GoURMET Sentence Pairs Evaluation",
+                   project_name="gourmet", component_name="sentence-pairs-evaluation")
 print(t.to_json())

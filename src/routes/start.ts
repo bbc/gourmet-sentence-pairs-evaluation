5fb327e30880981dc509165f35d7d56eb15232ab
Susie Coleman <susiecoleman@users.noreply.github.com>
2019-10-16 12:09:03 +0100

    Switch from free text for evaluator Id selection to dropdown

--- src/routes/start.ts
+++ src/routes/start.ts
@@ -4,9 +4,37 @@ import { getSentenceSets } from '../DynamoDB/dynamoDBApi';
 const buildStartRoute = (app: Application) => {
   app.get('/start', (req: Request, res: Response) => {
     getSentenceSets().then(sentenceSets => {
-      res.render('start', { sentenceSets });
+      res.render('start', { sentenceSets, evaluatorIds });
     });
   });
 };
+
+const evaluatorIds = [
+  'tester',
+  'BBC_Bulgarian_01',
+  'BBC_Bulgarian_02',
+  'BBC_Bulgarian_03',
+  'BBC_Bulgarian_04',
+  'BBC_Swahili_01',
+  'BBC_Swahili_02',
+  'BBC_Swahili_03',
+  'BBC_Swahili_04',
+  'BBC_Gujarati_01',
+  'BBC_Gujarati_02',
+  'BBC_Gujarati_03',
+  'BBC_Gujarati_04',
+  'DW_Bulgarian_01',
+  'DW_Bulgarian_02',
+  'DW_Bulgarian_03',
+  'DW_Bulgarian_04',
+  'DW_Swahili_01',
+  'DW_Swahili_02',
+  'DW_Swahili_03',
+  'DW_Swahili_04',
+  'DW_Gujarati_01',
+  'DW_Gujarati_02',
+  'DW_Gujarati_03',
+  'DW_Gujarati_04',
+];
 
 export { buildStartRoute };

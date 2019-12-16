b7ea8713439a2ad9b8d3b8bc8e48e63362824374
Susie Coleman <login@susiecoleman.me>
2019-12-16 10:49:19 +0000

    test: Add tests for submitting dataset

--- src/__tests__/processDataset.test.ts
+++ src/__tests__/processDataset.test.ts
@@ -1,7 +1,10 @@
-import { cleanData } from '../processDataset';
+import { cleanData, submitDataset } from '../processDataset';
 import { Dataset, Language } from '../models/models';
 import { DatasetBody, DatasetFile } from '../models/requests';
 import { Some, None } from '../models/generics';
+
+jest.mock('../DynamoDB/dynamoDBApi');
+const dynamoDBApi = require('../DynamoDB/dynamoDBApi');
 
 describe('cleanData', () => {
   test('should return a Some of type Dataset if dataset body is valid', () => {
@@ -233,5 +236,101 @@ describe('cleanData', () => {
     );
     const expectedOutput = new Some(expectedDataset);
     expect(cleanData(input, file)).toEqual(expectedOutput);
+  });
+});
+
+describe('submitDataset', () => {
+  test('should return a promise of a string where the string is the sentence set ID generated when the data is submitted to the DB if data can be cleaned', () => {
+    const datasetIDInDB = '1234';
+
+    dynamoDBApi.putSentenceSetAndPairs.mockImplementation(() => {
+      return Promise.resolve(datasetIDInDB);
+    });
+
+    const input: DatasetBody = {
+      setName: '',
+      sourceLanguage: 'ENGLISH',
+      targetLanguage: 'BULGARIAN',
+    };
+    const file: DatasetFile = {
+      sourceText: 'Sentence 1.\nSentence 2.',
+      machineTranslatedText: 'Sentence 1.\nSentence 2.',
+      humanTranslatedText: 'Sentence 1.\nSentence 2.',
+    };
+
+    const mockCleaningFunction = (body: DatasetBody, file: DatasetFile) =>
+      new Some(
+        new Dataset([], [], [], '', Language.BULGARIAN, Language.BULGARIAN)
+      );
+
+    return submitDataset(input, file, mockCleaningFunction).then(id => {
+      expect(id).toEqual(datasetIDInDB);
+    });
+  });
+
+  test('should return a promise that will reject if the cleaned data is an instance of None', () => {
+    const datasetIDInDB = '1234';
+
+    dynamoDBApi.putSentenceSetAndPairs.mockImplementation(() => {
+      return Promise.resolve(datasetIDInDB);
+    });
+
+    const input: DatasetBody = {
+      setName: '',
+      sourceLanguage: 'ENGLISH',
+      targetLanguage: 'BULGARIAN',
+    };
+
+    // Cleaned data will be none no source text
+    const file: DatasetFile = {
+      sourceText: '',
+      machineTranslatedText: 'Sentence 1.\nSentence 2.',
+      humanTranslatedText: 'Sentence 1.\nSentence 2.',
+    };
+
+    const mockCleaningFunction = (body: DatasetBody, file: DatasetFile) =>
+      new None();
+
+    return expect(
+      submitDataset(input, file, mockCleaningFunction)
+    ).rejects.toMatch(
+      'Could not clean data. Dataset:{"setName":"","sourceLanguage":"ENGLISH","targetLanguage":"BULGARIAN"}'
+    );
+  });
+
+  test('should return a promise that will reject if the cleaned data Option does not contain a value that is of type DataSet', () => {
+    const datasetIDInDB = '1234';
+
+    dynamoDBApi.putSentenceSetAndPairs.mockImplementation(() => {
+      return Promise.resolve(datasetIDInDB);
+    });
+
+    jest.mock('../processDataset');
+    const dataset = require('../processDataset');
+    dataset.cleanData.mockImplementation(() => {
+      return new Some('123');
+    });
+
+    const input: DatasetBody = {
+      setName: '',
+      sourceLanguage: 'ENGLISH',
+      targetLanguage: 'BULGARIAN',
+    };
+
+    // Cleaned data will be none no source text
+    const file: DatasetFile = {
+      sourceText: '',
+      machineTranslatedText: 'Sentence 1.\nSentence 2.',
+      humanTranslatedText: 'Sentence 1.\nSentence 2.',
+    };
+
+    const mockCleaningFunction = (body: DatasetBody, file: DatasetFile) =>
+      new Some('');
+
+    return expect(
+      submitDataset(input, file, mockCleaningFunction)
+    ).rejects.toMatch(
+      'Could not clean data. Dataset:{"setName":"","sourceLanguage":"ENGLISH","targetLanguage":"BULGARIAN"}'
+    );
   });
 });

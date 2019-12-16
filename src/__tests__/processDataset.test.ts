72ea9df1394405aebe123c6df7c05a0653c0750c
Susie Coleman <login@susiecoleman.me>
2019-12-16 09:04:46 +0000

    test: Add additional tests for cleaning data uploaded

--- src/__tests__/processDataset.test.ts
+++ src/__tests__/processDataset.test.ts
@@ -51,6 +51,54 @@ describe('cleanData', () => {
     expect(cleanData(input, file)).toEqual(expectedOutput);
   });
 
+  test('should not create sentences that are empty strings when multiple carriage returns are used to split data', () => {
+    const input: DatasetBody = {
+      setName: 'test',
+      sourceLanguage: 'ENGLISH',
+      targetLanguage: 'BULGARIAN',
+    };
+    const file: DatasetFile = {
+      sourceText: 'Sentence 1.\r\r\r\nSentence 2.\r\n',
+      humanTranslatedText: 'Sentence 1.\r\nSentence 2.\r\n',
+      machineTranslatedText: 'Sentence 1.\r\nSentence 2.\r\n',
+    };
+    const expectedOutput = new Some(
+      new Dataset(
+        ['Sentence 1.', 'Sentence 2.'],
+        ['Sentence 1.', 'Sentence 2.'],
+        ['Sentence 1.', 'Sentence 2.'],
+        'test',
+        Language.ENGLISH,
+        Language.BULGARIAN
+      )
+    );
+    expect(cleanData(input, file)).toEqual(expectedOutput);
+  });
+
+  test('should not create sentences that are empty strings when multiple new lines are used to split data', () => {
+    const input: DatasetBody = {
+      setName: 'test',
+      sourceLanguage: 'ENGLISH',
+      targetLanguage: 'BULGARIAN',
+    };
+    const file: DatasetFile = {
+      sourceText: 'Sentence 1.\n\n\n\nSentence 2.\r\n',
+      humanTranslatedText: 'Sentence 1.\r\nSentence 2.\r\n',
+      machineTranslatedText: 'Sentence 1.\r\nSentence 2.\r\n',
+    };
+    const expectedOutput = new Some(
+      new Dataset(
+        ['Sentence 1.', 'Sentence 2.'],
+        ['Sentence 1.', 'Sentence 2.'],
+        ['Sentence 1.', 'Sentence 2.'],
+        'test',
+        Language.ENGLISH,
+        Language.BULGARIAN
+      )
+    );
+    expect(cleanData(input, file)).toEqual(expectedOutput);
+  });
+
   test('should return a None if machine translated text does not have enough sentences', () => {
     const input: DatasetBody = {
       setName: '',
@@ -108,6 +156,82 @@ describe('cleanData', () => {
       humanTranslatedText: 'Sentence 1.\nSentence 2.\nSentence 3.',
     };
     const expectedOutput = new None();
+    expect(cleanData(input, file)).toEqual(expectedOutput);
+  });
+
+  test("should return None if the source language is not defined in the 'Language' enum", () => {
+    const input: DatasetBody = {
+      setName: '',
+      sourceLanguage: 'GREEK',
+      targetLanguage: 'BULGARIAN',
+    };
+    const file: DatasetFile = {
+      sourceText: 'Sentence 1.\nSentence 2.',
+      machineTranslatedText: 'Sentence 1.\nSentence 2.',
+      humanTranslatedText: 'Sentence 1.\nSentence 2.',
+    };
+    const expectedOutput = new None();
+    expect(cleanData(input, file)).toEqual(expectedOutput);
+  });
+
+  test("should return None if the target language is not defined in the 'Language' enum", () => {
+    const input: DatasetBody = {
+      setName: '',
+      sourceLanguage: 'ENGLISH',
+      targetLanguage: 'GREEK',
+    };
+    const file: DatasetFile = {
+      sourceText: 'Sentence 1.\nSentence 2.',
+      machineTranslatedText: 'Sentence 1.\nSentence 2.',
+      humanTranslatedText: 'Sentence 1.\nSentence 2.',
+    };
+    const expectedOutput = new None();
+    expect(cleanData(input, file)).toEqual(expectedOutput);
+  });
+
+  test('should not fail if the source language is lower case', () => {
+    const input: DatasetBody = {
+      setName: '',
+      sourceLanguage: 'english',
+      targetLanguage: 'BULGARIAN',
+    };
+    const file: DatasetFile = {
+      sourceText: 'Sentence 1.\nSentence 2.',
+      machineTranslatedText: 'Sentence 1.\nSentence 2.',
+      humanTranslatedText: 'Sentence 1.\nSentence 2.',
+    };
+    const expectedDataset: Dataset = new Dataset(
+      ['Sentence 1.', 'Sentence 2.'],
+      ['Sentence 1.', 'Sentence 2.'],
+      ['Sentence 1.', 'Sentence 2.'],
+      '',
+      Language.ENGLISH,
+      Language.BULGARIAN
+    );
+    const expectedOutput = new Some(expectedDataset);
+    expect(cleanData(input, file)).toEqual(expectedOutput);
+  });
+
+  test('should not fail if the target language is lower case', () => {
+    const input: DatasetBody = {
+      setName: '',
+      sourceLanguage: 'ENGLISH',
+      targetLanguage: 'bulgarian',
+    };
+    const file: DatasetFile = {
+      sourceText: 'Sentence 1.\nSentence 2.',
+      machineTranslatedText: 'Sentence 1.\nSentence 2.',
+      humanTranslatedText: 'Sentence 1.\nSentence 2.',
+    };
+    const expectedDataset: Dataset = new Dataset(
+      ['Sentence 1.', 'Sentence 2.'],
+      ['Sentence 1.', 'Sentence 2.'],
+      ['Sentence 1.', 'Sentence 2.'],
+      '',
+      Language.ENGLISH,
+      Language.BULGARIAN
+    );
+    const expectedOutput = new Some(expectedDataset);
     expect(cleanData(input, file)).toEqual(expectedOutput);
   });
 });

4fd27652248a120e94324cf1653e705052362c9a
Susie Coleman <login@susiecoleman.me>
2019-12-20 15:03:49 +0000

    feat: Add evaluation coordinator link to access export datasets page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,10 @@ import { Request, Response, Application } from 'express';
 
 const buildIndexRoute = (app: Application) => {
   app.get('/', (req: Request, res: Response) => {
-    res.render('index', { datasetSubmissionUrl: '/dataset' });
+    res.render('index', {
+      datasetSubmissionUrl: '/dataset',
+      exportDataUrl: '/exportData',
+    });
   });
 };
 

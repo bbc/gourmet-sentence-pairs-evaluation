095051dfb8bc02e733640b3a05547c159078639a
Susie Coleman <login@susiecoleman.me>
2020-01-06 16:48:19 +0000

    feat: script to format data for sentence pair tool now produces JSON of the correct format to upload to the tool.

--- scripts/randomiseAndFormatData.py
+++ scripts/randomiseAndFormatData.py
@@ -1,22 +1,58 @@
 import sys
 import random
+import json
 
-# Script for randomising evaluation data sentence order. The filenames of the data must
-# be specified on lines 17,18,19.
+"""
+Script for randomising evaluation data sentence order, formatting it as JSON and
+tagging the sentences so the data can be imported into the Sentence Pairs Evaluation Tool.
 
-# This script takes a series of articles that have an original article
-# a human translated version of the article and a machine translated article split
-# across three text files. The script returns 3 files (original, human translation and
-# machine translation) which contain all the sentences from the articles provided and
-#  where the order of the sentences is consistent across the files i.e. sentence 1 in
-# the human translation file will be a translation of sentence 1 in the original file.
-# The sentences will have been shuffled so that they appear in a random order as opposed
-# to the order in the original articles.
+Input:
+Text files containing an original sentence, the human translation and a machine translation.
+There can be multiple sentences per file. The names of the files must be specified on lines 51,52 
+and 53. A JSON file with the external collaboration sentences. The file name must be specified on 
+line 55 and the JSON is in the form:
+
+{
+    "sentences": list<Sentences>
+}
+
+Where Sentences is of type:
+
+{
+    "original": string,
+    "humanTranslation": string,
+    "machineTranslation": string,
+}
+
+Output:
+A json file
+
+{
+    "sentences": list<Sentences>
+}
+
+Where Sentences is of type:
+
+{
+    "original": string,
+    "humanTranslation": string,
+    "machineTranslation": string,
+    "sentencePairType: string
+}
+
+The first 5 sentences will be marked as internal callibration ('C'). These are to get
+evaluators familiar with the task. The remaining sentences will be a mix of valid sentences ('A')
+i.e. sentences where the translation has been performed by a machine and external callibration
+sentences ('B') i.e. contrived examples.
+"""
+
 
 # Specify the files that are to be read in. The list must be in the same order for original, human translation and machine translation.
-original_filenames = []
-human_translation_filenames = []
-machine_translation_filenames = []
+original_filenames = ["bulgarian1EN.txt"]
+human_translation_filenames = ["bulgarian1HT.txt"]
+machine_translation_filenames = ["bulgarian1MT.txt"]
+
+external_callibration_json_file = "perfect.json"
 
 if(len(original_filenames) != len(human_translation_filenames) or len(human_translation_filenames) != len(machine_translation_filenames)):
     sys.exit("Files do not exist for every variation of the articles.")
@@ -49,23 +85,39 @@ for file_set in file_sets:
     for i in range(len(original)):
         sentences.append({
             "original": original[i],
-            "human_translation": human_translation[i],
-            "machine_translation": machine_translation[i]
+            "humanTranslation": human_translation[i],
+            "machineTranslation": machine_translation[i]
         })
 
 # Randomise the order of the sentences
 random.shuffle(sentences)
 
-# Write the shuffled sentences back to files maintaining the order of the sentences across the files. i.e. sentence 1 in all files will have the same meaning.
-original = open("original.txt", 'w')
-human_translation = open("human_translation.txt", 'w')
-machine_translation = open("machine_translation.txt", 'w')
+# Split data into internal collaboration and valid sentences
+internal_callibration_sentences = sentences[:5]
+valid_sentences = sentences[5:]
 
-for sentence in sentences:
-    original.write(sentence['original'])
-    human_translation.write(sentence['human_translation'])
-    machine_translation.write(sentence['machine_translation'])
+# Tag sentences
 
-original.close()
-human_translation.close()
-machine_translation.close()
+for s in internal_callibration_sentences:
+    s['sentencePairType'] = 'C'
+
+for s in valid_sentences:
+    s['sentencePairType'] = 'A'
+
+# Import and tag external callibration sentences
+
+with open(external_callibration_json_file) as f:
+    external_callibration_sentences = json.load(f)['sentences']
+
+for s in external_callibration_sentences:
+    s['sentencePairType'] = 'B'
+
+# Combine the valid sentences and external callibration sentences and shuffle
+valid_and_external_callibration_sentences = valid_sentences + external_callibration_sentences
+random.shuffle(valid_and_external_callibration_sentences)
+
+evaluationData = open('evaluationData.json', 'w')
+
+evaluationData.write(json.dumps({"sentences": internal_callibration_sentences + valid_and_external_callibration_sentences}))
+
+evaluationData.close()
